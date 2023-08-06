c993877dbd73b5c46ef65a4a32635123b364113e
totuanan <an.to@primedata.ai>
2023-08-06 12:05:37 +0700

    MAT block

--- models/networks/normalization.py
+++ models/networks/normalization.py
@@ -49,6 +49,60 @@ def get_nonspade_norm_layer(opt, norm_type='instance'):
 
     return add_norm_layer
 
+class SignWithSigmoidGrad(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x):
+        result = (x > 0).float()
+        sigmoid_result = torch.sigmoid(x)
+        ctx.save_for_backward(sigmoid_result)
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_result):
+        (sigmoid_result,) = ctx.saved_tensors
+        if ctx.needs_input_grad[0]:
+            grad_input = grad_result * sigmoid_result * (1 - sigmoid_result)
+        else:
+            grad_input = None
+        return grad_input
+
+def dynamic_attention(q, k, q_prune, k_prune, v, smooth=None, v2=None):
+    # q, k, v: b, c, h, w
+    b, c_qk, h_q, w_q = q.shape
+    h_kv, w_kv = k.shape[2:]
+    q = q.view(b, c_qk, h_q * w_q).transpose(-1, -2).contiguous()
+    k = k.view(b, c_qk, h_kv * w_kv)
+    v = v.view(b, -1, h_kv * w_kv).transpose(-1, -2).contiguous()
+    q_prune = q_prune.view(b, -1, h_q * w_q).transpose(-1, -2).contiguous()
+    k_prune = k_prune.view(b, -1, h_kv * w_kv)
+    mask = SignWithSigmoidGrad.apply(torch.bmm(q_prune, k_prune) / k_prune.shape[1])
+    # q: b, N_q, c_qk
+    # k: b, c_qk, N_kv
+    # v: b, N_kv, c_v
+    if smooth is None:
+        smooth = c_qk ** 0.5
+    cor_map = torch.bmm(q, k) / smooth
+    attn = torch.softmax(cor_map, dim=-1)
+    # attn: b, N_q, N_kv
+    masked_attn = attn * mask
+    output = torch.bmm(masked_attn, v)
+    # output: b, N_q, c_v
+    output = output.transpose(-1, -2).contiguous().view(b, -1, h_q, w_q)
+    conf = masked_attn.sum(-1).view(b, 1, h_q, w_q)
+
+    # conf_map = torch.max(cor_map, -1, keepdim=True)[0]
+    # conf_map = (conf_map - conf_map.mean(dim=1, keepdim=True)).view(b, 1, h_q, w_q)
+    # conf_map = torch.sigmoid(conf_map * 10.0)
+
+    if v2 is not None:
+        v2 = v2.view(b, -1, h_kv * w_kv).transpose(-1, -2).contiguous()
+        output2 = torch.bmm(torch.softmax(torch.masked_fill(cor_map, mask.bool(), -1e4), dim=-1),
+                            v2).transpose(-1, -2).contiguous().view(b, -1, h_q, w_q)
+    else:
+        output2 = None
+    return output, cor_map, conf, output2
+
 
 # Creates SPADE normalization layer based on the given configuration
 # SPADE consists of two steps. First, it normalizes the activations using
@@ -90,9 +144,17 @@ class SPADE(nn.Module):
         #for real map
         self.real_conv = nn.Conv2d(in_channels=3, out_channels=label_nc, kernel_size=3, padding=1)
 
-        self.query = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
-        self.key = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
-        self.value = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
+        # self.query = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
+        # self.key = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
+        # self.value = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
+        # self.query = nn.Conv2d(label_nc, nhidden, kernel_size=1, stride=1)
+        # self.key = nn.Conv2d(label_nc, nhidden, kernel_size=1, stride=1)
+        # self.value = nn.Conv2d(label_nc, nhidden, kernel_size=1, stride=1)
+        self.f = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
+        self.g = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
+        self.h = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
+        self.f_prune = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
+        self.g_prune = nn.Conv2d(label_nc, nhidden, kernel_size=ks, stride=1, padding=pw)
 
         self.alpha = nn.Parameter(torch.Tensor([1]))
 
@@ -115,12 +177,20 @@ class SPADE(nn.Module):
         realmap = F.interpolate(realmap, size=x.size()[2:], mode='nearest')
         realmap = self.real_conv(realmap)
 
-        f, g, h = self.query(segmap), self.key(realmap), self.value(realmap)
-        f, g, h = self.relu(f), self.relu(g), self.relu(h)
-        f_g = F.softmax(torch.matmul(f.transpose(-2, -1), g))
+        # print("Segmap: ", segmap.shape)
+        # print("Realmap: ", realmap.shape)
+
+        attn_output, cor_map, conf, output2 = dynamic_attention(
+            self.f(segmap), self.g(realmap), self.f_prune(segmap), self.g_prune(realmap), self.h(realmap), None, None)
+        # f, g, h = self.query(segmap), self.key(realmap), self.value(realmap)
+        # f, g, h = self.relu(f), self.relu(g), self.relu(h)
+        # f_g = F.softmax(torch.matmul(f.transpose(-2, -1), g))
         # #
-        actv_beta = self.alpha * torch.matmul(h, f_g)
-        actv_beta = self.relu(actv_beta)
+        # actv_beta = self.alpha * torch.matmul(h, f_g)
+        # actv_beta = self.relu(actv_beta)
+
+        actv_beta = self.relu(attn_output)
+        # print(actv_beta.shape)
 
         # actv_beta = self.mlp_shared(segmap)
         #print("actv:", actv_beta)
